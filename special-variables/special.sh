672435eb97a0ed85f3e53da4241697621da4ea4d
hari sankar <harisan@thoughtworks.com>
2019-11-25 10:07:10 +0530

    bash spacial variables

---
+++ special-variables/special.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "process id of shell $$"
+echo "file name of current script = $0"
+echo "Number of arguments to to script = $#"
+echo "Argument 1 = $1"
+echo "Argument 2 = $2"
+echo "all the arguments to the scrpit = $*"
+echo "all the arguments to the scrpit = $@"
